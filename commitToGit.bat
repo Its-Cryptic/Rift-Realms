74dd765287c39cfeaba0a4d29792b9d2b56be9ce
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 12:42:30 -0700

    added .gitignore

--- commitToGit.bat
+++ commitToGit.bat
@@ -2,7 +2,7 @@
 git add .
 set /p commitMessage="Enter commit message: "
 git commit -m "%commitMessage%"
-git push origin HEAD:master
+git push origin HEAD:main
 echo.
 echo Commit and push complete.
 pause
