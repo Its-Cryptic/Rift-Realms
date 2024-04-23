ae28e3a6d9b9fadbd44dcacbb86e5265e78d4207
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 13:05:37 -0700

    Updated .gitignore

--- commitToGit.bat
+++ commitToGit.bat
@@ -1,8 +1,11 @@
 @echo off
 git add .
+Color 2
 set /p commitMessage="Enter commit message: "
 git commit -m "%commitMessage%"
+Color 6
 git push origin HEAD:main
 echo.
+Color Green
 echo Commit and push complete.
 pause
